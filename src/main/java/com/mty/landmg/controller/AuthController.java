cb8cdb03a5e9d9f60c12b3f9f037a58cf90b1111
york <315895718@qq.com>
2025-02-18 17:09:03 +0800

    #york refine

--- src/main/java/com/mty/landmg/controller/AuthController.java
+++ src/main/java/com/mty/landmg/controller/AuthController.java
@@ -2,19 +2,18 @@ package com.mty.landmg.controller;
 
 import com.mty.landmg.dto.AuthDTO;
 import com.mty.landmg.dto.AuthResDTO;
+import com.mty.landmg.entity.User;
 import com.mty.landmg.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.Map;
 
 /**
@@ -42,8 +41,10 @@ public class AuthController {
     @PostMapping("/login")
     public AuthResDTO authByPassword(@RequestBody AuthDTO auth) {
         var authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(auth.getCode(), auth.getPwd()));
-        UserDetails userDetails = (UserDetails) authenticate.getPrincipal();
-        return new AuthResDTO(auth.getCode(), jwtUtil.createToken(auth.getCode(), Map.of("roles", AuthorityUtils.authorityListToSet(userDetails.getAuthorities()))));
+        User userDetails = (User) authenticate.getPrincipal();
+        return new AuthResDTO(auth.getCode(),
+                jwtUtil.createToken(auth.getCode(), Map.of("roles", AuthorityUtils.authorityListToSet(userDetails.getAuthorities())
+        ,"id",userDetails.getMemberId())));
     }
 
     //todo token接入redis后实现退出功能
--- src/main/java/com/mty/landmg/service/impl/UserServiceImpl.java
+++ src/main/java/com/mty/landmg/service/impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,7 +28,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
         log.debug("数据库认证：{}", username);
         User one = this.getOne(Wrappers.<User>lambdaQuery().eq(User::getMemberCode, username));
         log.debug("当前用户信息：{}", JSON.toJSONString(one));
