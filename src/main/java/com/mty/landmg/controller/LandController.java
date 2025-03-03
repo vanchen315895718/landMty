0f93ab99c225a91d8fd39a1d0017c22d9c98624f
york <315895718@qq.com>
2025-03-03 09:17:35 +0800

    #york add update & pagelist

--- src/main/java/com/mty/landmg/controller/LandController.java
+++ src/main/java/com/mty/landmg/controller/LandController.java
@@ -1,12 +1,15 @@
 package com.mty.landmg.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mty.landmg.dto.LandDTO;
+import com.mty.landmg.entity.Land;
 import com.mty.landmg.service.ILandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * 用户管理
+ * 地块管理
  *
  * @author york
  * @since 2025-02-25
@@ -25,5 +28,34 @@ public class LandController {
     @DeleteMapping("/del/{id}")
     public boolean del(@RequestParam("id") Long id) {
         return landService.removeById(id);
+    }
+
+    /**
+     * 修改地块信息
+     *
+     * @param landDTO 地块DTO
+     * @return 是否修改成功
+     */
+    @PutMapping("/update")
+    public boolean update(@RequestBody LandDTO landDTO) {
+        Land land = landDTO.toLand();
+        return landService.updateById(land);
+    }
+
+    /**
+     * 分页查询地块信息
+     *
+     * @param pageNum  当前页码
+     * @param pageSize 每页数量
+     * @return 分页结果
+     */
+    @GetMapping("/listPage")
+    public Page<Land> list(@RequestParam(defaultValue = "1") int pageNum,
+                           @RequestParam(defaultValue = "10") int pageSize,
+                           @RequestParam String remark) {
+        Page<Land> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<Land> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("remark",remark);
+        return landService.page(page, queryWrapper);
     }
 }
